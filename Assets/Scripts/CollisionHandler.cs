b1777c62ca3e8eae6671cf300ab9ce56284f4848
Roman <r.demyanov.anthill@gmail.com>
2024-10-09 20:34:53 +0300

    review updates

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,53 +1,47 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer), typeof(Rigidbody))]
+[RequireComponent(typeof(Renderer))]
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] private string _respawnTag = "Respawn";
     [SerializeField] private string _collisionTag = "Collision";
 
-    private bool _hasCollision = false;
+	private bool _hasCollision = false;
 
-    private void ChangeColor()
-    {
-        Color color = new Color(
-            Random.Range(0f, 1f),
-            Random.Range(0f, 1f),
-            Random.Range(0f, 1f)
-         );
-
-        gameObject.GetComponent<Renderer>().material.color = color;
-    }
-
-    private void ReturnToPool()
-    {
-        var pool = GameObject.FindWithTag(_respawnTag);
-
-        if (pool != null)
-        {
-            if (pool.TryGetComponent(out Spawner spawner))
-            {
-                spawner._pool.Release(gameObject);
-            }
-        }
-    }
+    public event Action<GameObject> Died;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == _collisionTag && _hasCollision == false)
+        if (collision.gameObject.tag == _collisionTag && !_hasCollision)
         {
-            int secondsToDissapear = Random.Range(2, 6);
-
-            ToggleCollision();
-            ChangeColor();
-            Invoke(nameof(ReturnToPool), secondsToDissapear);
-        }
+			int secondsToDissapear = UnityEngine.Random.Range(2, 6);
+			ToggleCollision();
+			ChangeColor();
+			StartCoroutine(Dissapear(secondsToDissapear));
+		}
     }
 
-    public void ToggleCollision()
+    private IEnumerator Dissapear(int secondsToDissapear)
     {
-        _hasCollision = !_hasCollision;
-    }
+        yield return new WaitForSeconds(secondsToDissapear);
+		ToggleCollision();
+		Died?.Invoke(gameObject);
+	}
+
+	private void ToggleCollision()
+	{
+		_hasCollision = !_hasCollision;
+	}
+
+	private void ChangeColor()
+	{
+		Color color = new Color(
+			UnityEngine.Random.Range(0f, 1f),
+			UnityEngine.Random.Range(0f, 1f),
+			UnityEngine.Random.Range(0f, 1f)
+		 );
+
+		gameObject.GetComponent<Renderer>().material.color = color;
+	}
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -5,49 +6,57 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _prefab;
     [SerializeField] private float _repeatRate = 1f;
-    [SerializeField] private int _poolCapacity= 5;
+    [SerializeField] private int _poolCapacity = 5;
     [SerializeField] private int _poolMaxSize = 5;
     [SerializeField] private int _rangeOffset = 5;
 
     public ObjectPool<GameObject> _pool;
 
-    private void Awake()
+    public void Start()
     {
-        _pool = new ObjectPool<GameObject>(
-            createFunc: () => Instantiate(_prefab),
-            actionOnGet: (obj) => ActionOnGet(obj),
-            actionOnRelease: (obj) => ActionOnRelese(obj),
-            actionOnDestroy: (obj) => Destroy(obj),
-            collectionCheck: true,
-            defaultCapacity: _poolCapacity,
-            maxSize: _poolMaxSize);
+        InvokeRepeating(nameof(GetCube), 0.0f, _repeatRate);
     }
 
-    void Start()
-    {
-        InvokeRepeating(nameof(GetSphere), 0.0f, _repeatRate);
-    }
+	private void Awake()
+	{
+		_pool = new ObjectPool<GameObject>(
+			createFunc: () => Instantiate(_prefab),
+			actionOnGet: (obj) => ActionOnGet(obj),
+			actionOnRelease: (obj) => ActionOnRelese(obj),
+			actionOnDestroy: (obj) => Destroy(obj),
+			collectionCheck: true,
+			defaultCapacity: _poolCapacity,
+			maxSize: _poolMaxSize);
+	}
 
-    private void ActionOnGet(GameObject obj)
+	private void ActionOnGet(GameObject obj)
     {
         obj.transform.position = Random.insideUnitSphere * _rangeOffset + gameObject.transform.position;
-        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        if(obj.TryGetComponent(out Rigidbody rigidbody))
+        {
+			rigidbody.velocity = Vector3.zero;
+		};
+
+        if(obj.TryGetComponent(out CollisionHandler collisionHandler))
+        {
+			collisionHandler.Died += ActionOnRelese;
+		}
+       
         obj.SetActive(true);
     }
 
     private void ActionOnRelese(GameObject obj)
     {
-        obj.GetComponent<Renderer>().material.color = Color.white;
+		if(obj.TryGetComponent(out CollisionHandler collisionHandler))
+		{
+			collisionHandler.Died -= ActionOnRelese;
+		}
 
-        if (obj.TryGetComponent(out CollisionHandler collisionHandler))
-        {
-            collisionHandler.ToggleCollision();
-        }
-        
-        obj.SetActive(false);
+		obj.SetActive(false);
     }
 
-    private void GetSphere()
+    private void GetCube()
     {
         _pool.Get();
     }
